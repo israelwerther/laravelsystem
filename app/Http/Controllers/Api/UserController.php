9256b143e93ccf53f9df245fd54295c43b9f7d07
israelwerther <israelwerther48@outlook.com>
2023-09-21 16:15:52 -0300

    criando user corretamente

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate();
+
+        return UserResource::collection($users);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        $user = User::create($data);
+        return new UserResource($user);
+    }
+}
