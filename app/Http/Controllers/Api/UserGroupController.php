e1dde4b0544283a92794c16dc016c2485bd58a0c
israelwerther <israelwerther48@outlook.com>
2023-09-23 16:05:10 -0300

    correções

---
+++ app/Http/Controllers/Api/UserGroupController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateUserGroupRequest;
+use App\Http\Resources\UserGroupResource;
+use App\Models\UserGroup;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserGroupController extends Controller
+{
+    public function index()
+    {
+        $userGroups = UserGroup::all();
+        return UserGroupResource::collection($userGroups);
+    }
+    
+
+    public function store(StoreUpdateUserGroupRequest $request)
+    {
+        $data = $request->validated();
+        $userGroup = UserGroup::create($data);
+        return new UserGroupResource($userGroup);
+    }
+
+    public function show($id)
+    {        
+        $userGroup = UserGroup::findOrFail($id);
+        return new UserGroupResource($userGroup);
+    }
+
+    public function update(StoreUpdateUserGroupRequest $request, $id)
+    {
+        $userGroup = UserGroup::findOrFail($id);
+        $userGroup->update($request->all());
+        return $userGroup;
+    }
+
+    public function destroy(string $id)
+    {
+        $userGroup = UserGroup::findOrFail($id);
+        $userGroup->delete();
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\UserGroupController;
 use Illuminate\Support\Facades\Route;
 
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
@@ -8,6 +9,10 @@ Route::patch('/users/{id}', [UserController::class, 'update']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
+
+Route::apiResource('user-groups', UserGroupController::class);
+
+Route::post('users/{user_id}/groups', 'UserController@associateToGroup');
 
 Route::get('/', function () {
     return response()->json([
